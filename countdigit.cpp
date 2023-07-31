e712d8ceaa66386547c2d3919855bec148538435
Abhishek <124251095+Abhishek1158@users.noreply.github.com>
2023-07-31 22:28:26 +0530

    Add files via upload

---
+++ countdigit.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int digits=0;  //610
+    while(n>0){
+        digits++;   //1  , 2 , 3
+        n=n/10;     //61 , 6 , 0
+    }
+    cout<<digits<<endl;
+    return 0;
+}
